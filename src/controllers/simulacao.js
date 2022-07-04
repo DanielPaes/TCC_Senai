18f618805145456d0ee641a47c82ff14aeed3ec3
DanielPaes <daniel.p.aes@hotmail.com>
2022-07-04 00:18:59 -0300

    Simulação funcionando

--- src/controllers/simulacao.js
+++ src/controllers/simulacao.js
@@ -2,21 +2,43 @@ let btnValidarCartao = document.querySelector('#btn-validar-cartao');
 
 let inputCpf = document.querySelector('#valida-cpf');
 
+let pacienteIsolamento = false; 
+let usuarioCartaoAtivo = false;
+
+let acessoLiberado = false;
+
+
 btnValidarCartao?.addEventListener('click', async function(event){
     event.preventDefault();
+    try{
+        await fetch(`http://localhost:3000/pacientes/${inputCpf.value}`)
+            .then((data) => data.json())
+            .then((post) => { 
+                pacienteIsolamento = post[0].isolamento;
+        });
+    } catch(err){
+        alert("User not found 1.");
+        console.log(inputCpf.value);
+        cleanObject();
+    }
+
     try{
         await fetch(`http://localhost:3000/usuarios/${inputCpf.value}`)
             .then((data) => data.json())
             .then((post) => { 
-                if(post[1].cartao_ativo){
-                    alert('Acesso liberado.');
-                } else {
-                    alert('Cartão bloqueado. Uma mensagem foi enviada informando o motivo do bloqueio.')
-                }                           
-        });
+                usuarioCartaoAtivo = post[0].cartao_ativo});
     } catch(err){
-        alert("User not found.");
+        alert("User not found. 2");
         console.log(inputCpf.value);
-        cleanObject();
     }
+
+    console.log(pacienteIsolamento);
+    console.log(usuarioCartaoAtivo);
+
+    if(!pacienteIsolamento && usuarioCartaoAtivo){
+        alert('Acesso liberado');
+    } else {
+        alert('Cartão bloqueado');
+    }
+
 })
